b1f99607de293d0b8ff4f202dc35ae2c987349fd
felix_kuehn <felix_kuehn@mein.gmx>
2026-09-08 10:48:51 +0200

    Made it possible to change active days with the use of addEventListener("click)

--- script2.js
+++ script2.js
@@ -1,82 +1,8 @@
 'use strict';
-const date = new Date();
 let pickedDate = new Date();
 const baseURL = "https://history.muffinlabs.com/date";
 
-function reloadPage() {
-
-const day = date.getDate();
-const month = date.getMonth();
-const year = date.getFullYear();
-const weekDay = date.getDay();
-
-const pickedDay =pickedDate.getDate();
-const pickedMonth = pickedDate.getMonth();
-const pickedYear = pickedDate.getFullYear();
-const pickedWeekDay = pickedDate.getDay();
-
-console.log(date);
-console.log(day);
-console.log(month);
-console.log(year);
-console.log(weekDay);
-
-
-var apiURL = `${baseURL}/${pickedMonth + 1}/${pickedDay}`;
-console.log(apiURL);
-
-const fullDate =
-        `${String(day).padStart(2, '0')}.${String(month + 1).padStart(2, '0')}.${year}`;
-
-console.log(fullDate);
-
-function berechneTagesnummer(pickedDate) {
-    const jahresanfang = new Date(
-        pickedDate.getFullYear(),0,1);
-
-    const millisekundenProTag = 1000 * 60 * 60 * 24;
-
-    return Math.floor(
-        (pickedDate - jahresanfang) / millisekundenProTag) + 1;
-}
-
-function berechneTageUebrig(pickedDate) {
-    const jahresende = new Date(
-        pickedDate.getFullYear() + 1,
-        0,
-        1
-    );
-
-    const millisekundenProTag = 1000 * 60 * 60 * 24;
-
-    return Math.floor(
-        (jahresende - pickedDate) / millisekundenProTag
-    );
-}
-
-function berechneWocheImMonat(day) {
-    return Math.ceil(day / 7);
-}
-
-function berechneTageImMonat(year, month) {
-    const ersterTagNaechsterMonat =
-        new Date(year, month + 1, 1);
-
-    const ersterTagAktuellerMonat =
-        new Date(year, month, 1);
-
-    const millisekundenProTag = 1000 * 60 * 60 * 24;
-
-    return (
-        (ersterTagNaechsterMonat - ersterTagAktuellerMonat)
-        / millisekundenProTag
-    );
-}
-
-const tagesNummer = berechneTagesnummer(pickedDate);
-const tageUebrig = berechneTageUebrig(pickedDate);
-const wochentgz = berechneWocheImMonat(pickedDay);
-const daysInMonth = berechneTageImMonat(pickedYear, pickedMonth);
+console.log(pickedDate);
 
 const wochentage = [
     "Sonntag",
@@ -111,30 +37,54 @@ const daycounter = [
     "fünfte"
 ];
 
+function reloadPage() {
+
+const pickedDay =pickedDate.getDate();
+const pickedMonth = pickedDate.getMonth();
+const pickedYear = pickedDate.getFullYear();
+const pickedWeekDay = pickedDate.getDay();
+
+const tagesNummer = berechneTagesnummer(pickedDate);
+const tageUebrig = berechneTageUebrig(pickedDate);
+const wochentgz = berechneWocheImMonat(pickedDay);
+const daysInMonth = berechneTageImMonat(pickedYear, pickedMonth);
+
 const wochentag = wochentage[pickedWeekDay];
 const monat = monate[pickedMonth];
 const daycount = daycounter[wochentgz - 1];
+
+const apiURL = `${baseURL}/${pickedMonth + 1}/${pickedDay}`;
+
+getAPIData(apiURL);
+
+const fullDate =
+        `${String(pickedDay).padStart(2, '0')}.${String(pickedMonth + 1).padStart(2, '0')}.${pickedYear}`;
+
+console.log(fullDate);
+
 
 document.getElementById("ueberschrift").textContent =
     `Kalenderblatt vom ${fullDate}.`;
 
     document.getElementById("beschreibung").textContent =
-    `Der ${day}.${monat} ${year} ist ein ${wochentag} ` +
+    `Der ${pickedDay}.${monat} ${pickedYear} ist ein ${wochentag} ` +
     `und zwar der ${daycount} ${wochentag} im Monat ${monat} ` +
-    `des Jahres ${year}. Es handelt sich um den ${tagesNummer}. ` +
+    `des Jahres ${pickedYear}. Es handelt sich um den ${tagesNummer}. ` +
     `Tag des Jahres, was bedeutet, dass es noch ${tageUebrig} ` +
     `Tage bis zum Jahresende sind. Der Monat ${monat} ` +
     `hat insgesamt ${daysInMonth} Tage. ` +
     `Heute ist kein gesetzlicher Feiertag in Deutschland.`;
 
 document.getElementById("ereignisse-ueberschrift").textContent = 
-`Historische Ereignisse am ${day}. ${monat} ${year}`;
+`Historische Ereignisse am ${pickedDay}. ${monat} ${pickedYear}:`;
 
 
-createHistoricEventsList()
+createHistoricEventsList(apiURL);
 
 
 const tabelle = document.getElementById("tabelle");
+
+tabelle.innerHTML = "";
 
 erstelleKalender(
     tabelle,
@@ -146,14 +96,29 @@ erstelleKalender(
 const presentDayCell = document.querySelectorAll("#tabelle td");
 
 presentDayCell.forEach(zelle => {
-    if (zelle.textContent === String(day)) {
+    if (zelle.textContent === String(pickedDay)) {
         zelle.style.backgroundColor = "lightgreen";
     }
 });
 
+}
+
 document.getElementById("lastButton").addEventListener("click", goOneMonthBack);
 
 document.getElementById("forwardButton").addEventListener("click", goOneMonthForward);
+
+tabelle.addEventListener("click", (event) => {
+    if (event.target.tagName !== "TD") {
+        return;
+}
+
+    const selectedDay = parseInt(event.target.textContent);
+    if (isNaN(selectedDay))
+        return;
+     pickedDate.setDate(selectedDay);
+     reloadPage();
+});
+
 
 function erstelleKalender(tabelle, year, month, daysInMonth) {
 
@@ -205,13 +170,13 @@ function erstelleKalender(tabelle, year, month, daysInMonth) {
 function goOneMonthBack () {
     pickedDate.setMonth(pickedDate.getMonth() - 1);
     console.log("Rückwärts");
-    updateCalendar();
+    reloadPage();
 }
 
 function goOneMonthForward () {
     pickedDate.setMonth(pickedDate.getMonth() + 1);
     console.log("Vorwärts");
-    updateCalendar();
+    reloadPage();
 }
 
 function updateCalendar() {
@@ -228,7 +193,7 @@ function updateCalendar() {
     );
 }
 
-function getAPIData() {
+function getAPIData(apiURL) {
     return fetch(apiURL)
         .then(response => {
             if (!response.ok) {
@@ -238,7 +203,6 @@ function getAPIData() {
             return response.json();
         })
         .then(data => {
-            console.log(data);
 
             return data;
             
@@ -248,13 +212,17 @@ function getAPIData() {
         });
 }
 
-function createHistoricEventsList(events) {
-    getAPIData()
+function createHistoricEventsList(apiURL) {
+    getAPIData(apiURL)
         .then(data => {
           const events = data.data.Events;
           const deaths = data.data.Deaths;
           const births = data.data.Births;
         
+            document.querySelector(".ereignisseHistorisch").innerHTML = "";
+            document.querySelector(".tode").innerHTML = "";
+            document.querySelector(".geburten").innerHTML = "";
+
         for (let i = 0; i < 2; i++) { 
             const randomIndex = Math.floor(Math.random() * events.length);
             const event = events[randomIndex];
@@ -277,5 +245,49 @@ function createHistoricEventsList(events) {
             listItem.textContent = `${birth.year}: ${birth.text}`;
             document.querySelector(".geburten").appendChild(listItem);
         }       
-      }); 
+      })}; 
+
+      function berechneTagesnummer(pickedDate) {
+    const jahresanfang = new Date(
+        pickedDate.getFullYear(),0,1);
+
+    const millisekundenProTag = 1000 * 60 * 60 * 24;
+
+    return Math.floor(
+        (pickedDate - jahresanfang) / millisekundenProTag) + 1;
 }
+
+function berechneTageUebrig(pickedDate) {
+    const jahresende = new Date(
+        pickedDate.getFullYear() + 1,
+        0,
+        1
+    );
+
+    const millisekundenProTag = 1000 * 60 * 60 * 24;
+
+    return Math.floor(
+        (jahresende - pickedDate) / millisekundenProTag
+    );
+}
+
+function berechneWocheImMonat(day) {
+    return Math.ceil(day / 7);
+}
+
+function berechneTageImMonat(year, month) {
+    const ersterTagNaechsterMonat =
+        new Date(year, month + 1, 1);
+
+    const ersterTagAktuellerMonat =
+        new Date(year, month, 1);
+
+    const millisekundenProTag = 1000 * 60 * 60 * 24;
+
+    return (
+        (ersterTagNaechsterMonat - ersterTagAktuellerMonat)
+        / millisekundenProTag
+    );
+}
+
+reloadPage();
